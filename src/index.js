62d23e2959498aaec4c66dd2cc1820809263ebc7
elliotschep <elliot.schep@gmail.com>
2018-10-25 15:57:59 +0200

    Created foundation for backend using https://github.com/howtographql/graphql-js

--- src/index.js
+++ src/index.js
@@ -21,8 +21,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: 'src/generated/prisma.graphql',
-      endpoint: '__PRISMA_ENDPOINT__',
-      secret: 'mysecret123',
+      endpoint: 'http://localhost:4466',
       debug: true,
     }),
   }),
